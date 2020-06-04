1ec1a9b97ad340a8e8cbe71ab5e8304510d02138
deibetheprogrammer <45319861+deibetheprogrammer@users.noreply.github.com>
2020-06-04 17:19:42 -0600

    Juego lista salvo el destructor

--- Juego.cpp
+++ Juego.cpp
@@ -1,5 +1,7 @@
 #include "Juego.h"
 
+using std::cout;
+
 //CONSTRUCTORS
 Juego::Juego()
 {
@@ -31,24 +33,113 @@ void setJugadores(Jugador* jugadores) {
 }
 
 //FUNCIONES
-Juego::validarGanador() {
+int Juego::validarGanador() {
 	
+	int winner = 2;
+	
+	Jugador* j1 = jugadores[0];
+	Jugador* j2 = jugadores[1];
+	
+	//Validar si J1 ganó
+	
+	//horizontales
+	if(tablero[0][0] == j1->getMarca() && tablero[0][1] == j1->getMarca() && tablero[0][2] == j1->getMarca())
+		winner = 0;
+	
+	if(tablero[1][0] == j1->getMarca() && tablero[1][1] == j1->getMarca() && tablero[1][2] == j1->getMarca())
+		winner = 0;
+	
+	if(tablero[2][0] == j1->getMarca() && tablero[2][1] == j1->getMarca() && tablero[2][2] == j1->getMarca())
+		winner = 0;
+	
+	//verticales
+	if(tablero[0][0] == j1->getMarca() && tablero[1][0] == j1->getMarca() && tablero[2][0] == j1->getMarca())
+		winner = 0;
+	
+	if(tablero[0][1] == j1->getMarca() && tablero[1][1] == j1->getMarca() && tablero[2][1] == j1->getMarca())
+		winner = 0;
+	
+	if(tablero[0][2] == j1->getMarca() && tablero[1][2] == j1->getMarca() && tablero[2][2] == j1->getMarca())
+		winner = 0;
+	
+	//diagonales
+	if(tablero[0][0] == j1->getMarca() && tablero[1][1] == j1->getMarca() && tablero[2][2] == j1->getMarca())
+		winner = 0;
+		
+	if(tablero[0][2] == j1->getMarca() && tablero[1][1] == j1->getMarca() && tablero[2][0] == j1->getMarca())
+		winner = 0;
+	
+	//Validar si J2 ganó
+	
+	//horizontales
+	if(tablero[0][0] == j2->getMarca() && tablero[0][1] == j2->getMarca() && tablero[0][2] == j2->getMarca())
+		winner = 1;
+	
+	if(tablero[1][0] == j2->getMarca() && tablero[1][1] == j2->getMarca() && tablero[1][2] == j2->getMarca())
+		winner = 1;
+	
+	if(tablero[2][0] == j2->getMarca() && tablero[2][1] == j2->getMarca() && tablero[2][2] == j2->getMarca())
+		winner = 1;
+	
+	//verticales
+	if(tablero[0][0] == j2->getMarca() && tablero[1][0] == j2->getMarca() && tablero[2][0] == j2->getMarca())
+		winner = 1;
+	
+	if(tablero[0][1] == j2->getMarca() && tablero[1][1] == j2->getMarca() && tablero[2][1] == j2->getMarca())
+		winner = 1;
+	
+	if(tablero[0][2] == j2->getMarca() && tablero[1][2] == j2->getMarca() && tablero[2][2] == j2->getMarca())
+		winner = 1;
+	
+	//diagonales
+	if(tablero[0][0] == j2->getMarca() && tablero[1][1] == j2->getMarca() && tablero[2][2] == j2->getMarca())
+		winner = 1;
+		
+	if(tablero[0][2] == j2->getMarca() && tablero[1][1] == j2->getMarca() && tablero[2][0] == j2->getMarca())
+		winner = 1;
+		
+	delete j1;
+	delete j2;
+	
+	return winner;
+		
 }
 
-Juego::elegirGanador() {
+int Juego::elegirGanador() {
 	
+	if(jugadores[0]->getPuntos() > jugadores[1]->getPuntos()) {
+		cout << "\n\n"
+			 << jugadores[0]->getNombre() << " gana !"
+			 << "\n";
+		
+	} else {
+		cout << "\n\n"
+			 << jugadores[1]->getNombre() << " gana !"
+			 << "\n";
+	}
+	
+	return 0;
 }
 
-Juego::mostrarPuntuaciones() {
+void Juego::mostrarPuntuaciones() {
 	
+	cout << "\n\n"
+		 << "Puntos Jugador 1: " << jugadores[0]->getPuntos()
+		 << " | "
+		 << "Puntos Jugador 2: " << jugadores[1]->getPuntos();
 }
 
-Juego::imprimirJugadores() {
+void Juego::imprimirJugadores() {
 	
+	cout << "\n\n"
+		 << "Jugador 1: " << jugadores[0]->getNombre()
+		 << " | "
+		 << "Jugador 2: " << jugadores[1]->getNombre();
 }
 
 //DESTRUCTOR
 Juego::~Juego()
 {
-	
+	tablero.~Tablero();
+		
 }
