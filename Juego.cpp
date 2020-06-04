6fb82b5fd501f3e1abf6bebb00ce03fa173cc765
deibetheprogrammer <45319861+deibetheprogrammer@users.noreply.github.com>
2020-06-04 15:05:21 -0600

    Clase template de Juego

--- Juego.cpp
+++ Juego.cpp
@@ -1,9 +1,54 @@
 #include "Juego.h"
 
+//CONSTRUCTORS
 Juego::Juego()
 {
 }
 
+Juego::Juego(Tablero tablero, Jugador jugador1, Jugador jugador2) {
+	
+	this->tablero = tablero;
+	this->jugadores[0] = jugador1;
+	this->jugadores[1] = jugador2;
+}
+
+//GETTERS
+Tablero getTablero() {
+	return tablero;
+}
+		
+Jugador* getJugadores() {
+	return jugadores;
+}
+		
+//SETTERS
+void setTablero(Tablero tablero) {
+	this->tablero = tablero;
+}
+		
+void setJugadores(Jugador* jugadores) {
+	this->jugadores = jugadores;
+}
+
+//FUNCIONES
+Juego::validarGanador() {
+	
+}
+
+Juego::elegirGanador() {
+	
+}
+
+Juego::mostrarPuntuaciones() {
+	
+}
+
+Juego::imprimirJugadores() {
+	
+}
+
+//DESTRUCTOR
 Juego::~Juego()
 {
+	
 }
--- Juego.h
+++ Juego.h
@@ -1,12 +1,40 @@
 #ifndef JUEGO_H
 #define JUEGO_H
+#include "Tablero.h"
+#include "Jugador.h"
 
 class Juego
 {
 	public:
+		//CONSTRUCTORES
 		Juego();
+		
+		Juego(Tablero tablero, Jugador jugador1, Jugador jugador2);
+		
+		//FUNCIONES
+		int validarGanador();
+		int elegirGanador();
+		void mostrarPuntuaciones();
+		void imprimirJugadores();
+		
+		//GETTERS
+		Tablero getTablero();
+		
+		Jugador* getJugadores();
+		
+		//SETTERS
+		void setTablero(Tablero tablero);
+		
+		void setJugadores(Jugador* jugadores);
+		
+		//DESTRUCTORES
 		~Juego();
 	protected:
+		
+		//ATRIBUTOS
+		Tablero tablero;
+		Jugador* jugadores[2];
+		
 };
 
 #endif
